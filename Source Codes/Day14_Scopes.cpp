9fb3dad7245d969b95ae2463fcf9374260448f02
youssefzaki28 <36207599+youssefzaki28@users.noreply.github.com>
2022-01-17 16:07:22 +0200

    Added Day 14 - Scopes

---
+++ Source Codes/Day14_Scopes.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	// Add your code here
+    Difference(vector<int> a){
+        this->elements=a;
+    }
+    void computeDifference(){
+        sort(this->elements.begin(),this->elements.end());
+        this->maximumDifference = abs(this->elements.front()-this->elements.back());
+    }
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
